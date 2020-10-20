ecb5abfcbb6e9c8aa16a7faad96586716cd85128
alisena <alisenamudaber@gmail.com>
2020-10-20 13:01:42 +0100

    Initial Commit

---
+++ Twitter-clone-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Twitter-clone-ios
+//
+//  Created by Alisena Mudaber on 10/20/20.
+//  Copyright © 2020 Alisena Mudaber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
